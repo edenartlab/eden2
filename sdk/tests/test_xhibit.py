53aadda6501c2d850bb38963646fb07ea0013352
xander <xandersteenbruggex@gmail.com>
2024-09-16 16:58:17 +0200

    revert lora id

--- sdk/tests/test_xhibit.py
+++ sdk/tests/test_xhibit.py
@@ -4,7 +4,7 @@ eden_client = EdenClient()
 
 args = {   
     "prompt": "A professional photo of <concept> as a fashion model looking fresh, beautiful, youthful, easeful, bright.",
-    "lora": "66df0f78634f1d2968c89f98",
+    "lora": "66904ec042b902d8eb3b41e6",
     "look_image": "https://storage.googleapis.com/public-assets-xander/Random/remove/xhibit/test2.jpeg",
     "face_image": "https://storage.googleapis.com/public-assets-xander/Random/remove/xhibit/face.jpeg",
     "resolution": 1152
