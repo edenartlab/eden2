33ab9fe22fd03f7b00764e127e15ca8d522fa300
xander <xandersteenbruggex@gmail.com>
2024-09-17 22:42:09 +0200

    update beeple test

--- sdk/tests/test_beeple.py
+++ sdk/tests/test_beeple.py
@@ -3,8 +3,8 @@ from eden import EdenClient
 eden_client = EdenClient()
 
 args = {   
-    "prompt": "Donald Trump bleeding from his ears",
-    "seed": 42
+    "prompt": "a terrified, tiny Donal Trump running away from a giant, evil kamala harris monster that is taking over the white house",
+    #"seed": 0
 }
 
 response = eden_client.create(
