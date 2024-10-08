35335cfa1ce02225ecc60ef6fd86e9359a6fb750
genekogan <kogan.gene@gmail.com>
2024-10-08 13:32:45 -0400

    catch up tools

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,11 +3,13 @@ from .story.handler import story
 from .news.handler import news
 from .chat.handler import chat
 from .write.handler import write
+from .runway.handler import runway
 
 __all__ = [
     'reel',
     'story',
     'news',
     'chat',
-    'write'
+    'write',
+    'runway'
 ]
