d495b272c487306bfc3971bb4d90b307fecd668b
genekogan <kogan.gene@gmail.com>
2024-10-25 23:21:25 -0700

    update access in yaml

---
+++ tools/media_utils/handler.py
@@ -0,0 +1,47 @@
+"""
+
+
+
+image transormations
+- resize
+- crop
+- rotate
+- flip
+- skew ?
+- grayscale
+- blur
+- sharpen
+- brightness
+- color balance
+- gamma correction
+
+
+image composition
+- overlay
+- blend / merge
+- mask
+- add text
+- add watermark
+
+video transofrmations
+- trim / split
+- merge
+- adjust fps
+- transition effects
+- reverse 
+
+audio transofrmations
+- trim / split
+- merge
+- adjust volume
+- normalize / equalize
+- effects (reverb, echo, distortion, etc.)
+
+compositing
+- add audio to video
+- replace audio
+- replace video
+
+subtitling
+
+"""
