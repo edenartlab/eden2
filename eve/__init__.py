82cb9d9228342a290728be0fcbbd75a890630963
genekogan <kogan.gene@gmail.com>
2024-11-12 12:13:12 -0500

    init env vars

---
+++ eve/__init__.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+home_dir = str(Path.home())
+
+# Load env variables from ~/.eve if it exists
+eve_path = os.path.join(home_dir, '.eve')
+if os.path.exists(eve_path):
+    load_dotenv(eve_path)
+
+# Load env variables from .env file if it exists
+env_path = ".env"
+if os.path.exists(env_path):
+    load_dotenv(env_path, override=True)
+
+
+# if not os.getenv("OPENAI_API_KEY"):
+#     print("\033[93m⚠️  Warning!!! \033[91m OPENAI_API_KEY not set\033[0m")
